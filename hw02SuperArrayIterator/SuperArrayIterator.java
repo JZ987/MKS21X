1e3ec952d641648fa498a3feda03cf5e25878734
Jack Zheng <jzheng7@stuy.edu>
2016-12-04 15:58:27 -0500

    Latest version

--- hw02SuperArrayIterator/SuperArrayIterator.java
+++ hw02SuperArrayIterator/SuperArrayIterator.java
@@ -3,13 +3,29 @@ import java.util.*;
 
 public class SuperArrayIterator implements Iterator<String> {
 
-    private Iterator<String> superarray;
+    private int element;
+    private SuperArray superArray;
     
-    public SuperArrayIterator(){
-	superarray = new Iterator<String>;
+    public SuperArrayIterator(SuperArray superAry){
+	superArray = superAry;
+	element = 0;
     }
 
     public boolean hasNext(){
-	
-
+	return element < superArray.size();
+    }
+    
+    public String next(){
+	if (hasNext()){
+	    element++;
+	    return superArray.get(element - 1);
+	}
+        else {
+	    throw new NoSuchElementException();
+	}
+    }
+        
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
 }
