a5c7d517205f1866ecfb50cfc17b86c79e24a202
Jack Zheng <jzheng7@stuy.edu>
2016-12-03 23:51:04 -0500

    Iterator practice with SuperArray

---
+++ hw02SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class SuperArrayIterator implements Iterator<String> {
+
+    private Iterator<String> superarray;
+    
+    public SuperArrayIterator(){
+	superarray = new Iterator<String>;
+    }
+
+    public boolean hasNext(){
+	
+
+}
