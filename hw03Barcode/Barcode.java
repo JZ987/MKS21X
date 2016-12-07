391f4004948f82618147729fda28dc19c82c3394
Jack Zheng <jzheng7@stuy.edu>
2016-12-07 05:50:42 -0500

    New update

--- hw03Barcode/Barcode.java
+++ hw03Barcode/Barcode.java
@@ -10,14 +10,30 @@ public class Barcode implements Comparable<Barcode>{
     //               or zip contains a non digit
     //               _zip and _checkDigit are initialized.
     
-    public Barcode(String zip) {}
+    public Barcode(String zip){
+	if(zip.length() != 5 || zip.contains("[a-zA-Z]+")){
+	    throw RuntimeException();
+	}
+	_zip = zip;
+	_checkDigit = checkSum() % 10;
+	
+    }
 
     // postcondition: Creates a copy of a bar code.
-    public Barcode clone(){}
+    public Barcode clone(){
+	Barcode clone = new Barcode(_zip);
+	return clone;
+    }
 
 
     // postcondition: computes and returns the check sum for _zip
-    private int checkSum(){}
+    private int checkSum(){
+	int sum = 0;
+	for(int i = 0; i < _zip.length(); i++){
+	    sum += Integer.parseInt(_zip.charAt(i));
+	}
+	return sum;
+    }
 
     //postcondition: format zip + check digit + Barcode 
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
