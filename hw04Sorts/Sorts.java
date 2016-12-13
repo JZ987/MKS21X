aa2e97a60bbbaaad411d3956edf0dc85920f13e7
Jack Zheng <jzheng7@stuy.edu>
2016-12-13 05:52:55 -0500

    Complete selectionSort method

--- hw04Sorts/Sorts.java
+++ hw04Sorts/Sorts.java
@@ -9,11 +9,25 @@ public class Sorts{
      *@param data  the elements to be sorted.
      */
     public static void selectionSort(int[] data){ 
-	int minSoFar = 0;
+        int x = 0;
 	for(int i = 0; i < data.length(); i++){
-	    if(data[i] 
-	
-
+	    int minSoFar = data[i];
+	    for(int j = i; j < data.length(); j++){
+		if(minSoFar >= data[j]){
+		    minSoFar = data[j];
+		    x = j;
+		}
+	    }
+	    data[x] = data[i];
+	    data[i] = minSoFar;
+	}
     }
 
+
+    public static void main(String[] args){
+	
+	
+
+	
+    }
 }
