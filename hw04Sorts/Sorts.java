724b69742d4bee3d5611001306cd11a7762b6125
Jack Zheng <jzheng7@stuy.edu>
2016-12-13 05:32:09 -0500

    New assignment Sorts

---
+++ hw04Sorts/Sorts.java
@@ -0,0 +1,19 @@
+public class Sorts{
+
+    public static String name(){
+	return "09.Zheng.Jack";
+    }
+    
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){ 
+	int minSoFar = 0;
+	for(int i = 0; i < data.length(); i++){
+	    if(data[i] 
+	
+
+    }
+
+}
