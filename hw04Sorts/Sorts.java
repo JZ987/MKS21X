779f34f9e35b2e88cccd0698bdde470373ffb867
Jack Zheng <jzheng7@stuy.edu>
2016-12-14 05:49:40 -0500

    Start insertionSort function

--- hw04Sorts/Sorts.java
+++ hw04Sorts/Sorts.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sorts{
 
     public static String name(){
@@ -10,9 +12,9 @@ public class Sorts{
      */
     public static void selectionSort(int[] data){ 
         int x = 0;
-	for(int i = 0; i < data.length(); i++){
+	for(int i = 0; i < data.length; i++){
 	    int minSoFar = data[i];
-	    for(int j = i; j < data.length(); j++){
+	    for(int j = i; j < data.length; j++){
 		if(minSoFar >= data[j]){
 		    minSoFar = data[j];
 		    x = j;
@@ -23,11 +25,41 @@ public class Sorts{
 	}
     }
 
+    public static void insertionSort(int[] data){
+	int currentNum;
+	for(int i = 1; i < data.length; i++){
+	    currentNum = data[i];
+	    for(int j = i - 1; j >= 0; j--){
+		if(currentNum >= data[j]){
+		    for(int x = i; x > j; x++){
+			data[x] = data[x-1];
+		    }
+		    data[j] = currentNum;
+		}
+	    }
+	}
+    }
 
     public static void main(String[] args){
 	
-	
+	int[] data = {64, 25, 12, 22, 11};
+    
+	String blah = "{";
 
-	
+	for(int i = 0; i < data.length; i++){
+	    blah += data[i] + " ";
+	}
+
+	System.out.println(blah + "}");
+       	insertionSort(data);
+    
+	String bleh = "{";
+
+	for(int i = 0; i < data.length; i++){
+	    bleh += data[i] + " ";
+	}
+
+	System.out.println(bleh + "}");
+
     }
 }
