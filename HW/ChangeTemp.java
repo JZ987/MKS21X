f8eabe8cd797140d2aded8f3bccd4eced7040273
Jack Zheng <jzheng7@stuy.edu>
2016-12-20 08:14:49 -0500

    Finish Temperature Convertion

--- HW/ChangeTemp.java
+++ HW/ChangeTemp.java
@@ -10,9 +10,7 @@ public class ChangeTemp{
 
     public static void main(String[] args){
 	System.out.println(CtoF(100.0));
-	
-	
+	System.out.println(CtoF(-40.0));
+	System.out.println(FtoC(212.0));
     }
-    
-
 }
