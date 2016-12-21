f5b1f000e77e96641e13b77716ada685d158ed64
Jack Zheng <jzheng7@stuy.edu>
2016-12-21 05:54:07 -0500

    Set the layout

--- HW/TemperatureConvert/TemperatureConvert.java
+++ HW/TemperatureConvert/TemperatureConvert.java
@@ -4,7 +4,30 @@ import java.awt.event.*;
 
 public class TemperatureConvert extends JFrame implements ActionListener{
 
+    private Container pane;
 
+    private JButton toF, toC;
+    private JLabel label;
+    private JTextField textbox;
 
+    public void actionPerformed(ActionEvent e){
+	
 
+    }
+
+    public TemperatureConvert(){
+	this.setTitle("Temperature Conversion");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	pane = this.getContentPane();
+	pane.setLayout(new CardLayout());
+
+    }
+
+    public static void main(String[] args){
+	TemperatureConvert a = new TemperatureConvert();
+	a.setVisible(true);
+    }
 }
